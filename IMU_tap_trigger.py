0537544eb1e3b91a62028cd311779c9185e35098
David <32776482+dawei-liang@users.noreply.github.com>
2024-10-15 17:35:43 -0500

    Add files via upload

--- IMU_tap_trigger.py
+++ IMU_tap_trigger.py
@@ -11,23 +11,26 @@ import time, os
 import math
 import machine
 from lsm6dsox import LSM6DSOX
-import VL53L1X
-from machine import Pin, SPI, LED
+from vl53l1x import VL53L1X
+from machine import Pin, SPI, LED, I2C
 
 # Initialize the LSM6DSOX sensor
 spi = SPI(5)
 cs = Pin("PF6", Pin.OUT_PP, Pin.PULL_UP)
 lsm = LSM6DSOX(spi, cs)
 
+tof = VL53L1X(I2C(2))
+
 # Initialize the sensor parameters
 sr = 104   # sr of IMU, default is 104 Hz
 acc_window_t = 100   # window size to aggregate sensor data, ms
-window_acc_z = []
-window_acc_x = []
-window_acc_y = []
-window_gyro_x = []
-window_gyro_y = []
-window_gyro_z = []
+IMU_idx = ["time"]
+window_acc_x = ["acc_x"]
+window_acc_y = ["acc_y"]
+window_acc_z = ["acc_z"]
+window_gyro_x = ["gyr_x"]
+window_gyro_y = ["gyr_y"]
+window_gyro_z = ["gyr_z"]
 window_acc_z_tmp = []
 window_acc_x_tmp = []
 window_acc_y_tmp = []
@@ -39,43 +42,68 @@ window_gyro_z_tmp = []
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time=2000)
+sensor.skip_frames(time=1000)
 
 # Initialize the LED
 led = LED("LED_BLUE")
 capture_interval = 3000   # LED duration, ms
 last_capture_time = time.ticks_ms()
+led2 = LED("LED_GREEN")
 
 tap = 0   # tap indicator
 tap_count = 0
 flg_sleep = 0
 
-start_time = time.ticks_ms()
+
+start_time_for_note = time.ticks_ms()
 
 while True:
-    current_time = time.ticks_ms()
-    # get IMU data
-    accel_x, accel_y, accel_z = lsm.accel()
-    gyro_x, gyro_y, gyro_z = lsm.gyro()
-    #accel_x = math.sqrt(accel_x**2)
-    #accel_y = math.sqrt(accel_y**2)
-    #accel_z = math.sqrt(accel_z**2)
 
-    # if LED is on, keep waiting
-    #if flg_sleep == 1:
-    #    flg_sleep = 0
-    #    start_time = current_time
-    #    continue
+    current_time_for_note = time.ticks_ms()
+    # note the IMU readings for evert x milliseconds
+    if time.ticks_diff(current_time_for_note, start_time_for_note) > 15000:
+        led2.on()
+        with open('/tmp/IMU.csv','a') as file:
+                #for line in file:
+                str_to_save = "\n" + str(IMU_idx).strip("[]") + \
+                "\n" + str(window_acc_x).strip("[]") + \
+                "\n" + str(window_acc_y).strip("[]") + \
+                "\n" + str(window_acc_z).strip("[]") + \
+                "\n" + str(window_gyro_x).strip("[]") + \
+                "\n" + str(window_gyro_y).strip("[]") + \
+                "\n" + str(window_gyro_z).strip("[]")
+                file.write(str_to_save)
+                    #line_Str=line_Str.rstrip('\n')
+                    #line_Str=line_Str.rstrip('\r')
+        file.close()
+        # clear the saved window
+        IMU_idx = ["time"]
+        window_acc_x = ["acc_x"]
+        window_acc_y = ["acc_y"]
+        window_acc_z = ["acc_z"]
+        window_gyro_x = ["gyr_x"]
+        window_gyro_y = ["gyr_y"]
+        window_gyro_z = ["gyr_z"]
+        start_time_for_note = current_time_for_note
+        time.sleep_ms(1000)
+        led2.off()
 
     # aggregate IMU for every acc_window_t ms to improve robustness
-    if time.ticks_diff(current_time, start_time) <= acc_window_t:
+    start_time = time.ticks_ms()
+    current_time = start_time
+    while time.ticks_diff(current_time, start_time) <= acc_window_t:
+        # get IMU data
+        accel_x, accel_y, accel_z = lsm.accel()
+        gyro_x, gyro_y, gyro_z = lsm.gyro()
+        #print(time.ticks_diff(current_time, start_time))
         window_acc_z_tmp.append(accel_z)
         window_acc_x_tmp.append(accel_x)
         window_acc_y_tmp.append(accel_y)
         window_gyro_x_tmp.append(gyro_x)
         window_gyro_y_tmp.append(gyro_y)
         window_gyro_z_tmp.append(gyro_z)
-        continue
+        current_time = time.ticks_ms()
+
     # execute every acc_window_t ms
     acc_z_tmp = sum(window_acc_z_tmp) / len(window_acc_z_tmp)
     acc_x_tmp = sum(window_acc_x_tmp) / len(window_acc_x_tmp)
@@ -83,6 +111,9 @@ while True:
     gyro_x_tmp = sum(window_gyro_x_tmp) / len(window_gyro_x_tmp)
     gyro_y_tmp = sum(window_gyro_y_tmp) / len(window_gyro_y_tmp)
     gyro_z_tmp = sum(window_gyro_z_tmp) / len(window_gyro_z_tmp)
+    # new window to save
+    IMU_time_stamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    IMU_idx.append(IMU_time_stamp)
     window_acc_z.append(acc_z_tmp)
     window_acc_x.append(acc_x_tmp)
     window_acc_y.append(acc_y_tmp)
@@ -103,8 +134,7 @@ while True:
             tap = 0
             tap_wait = 0
     # tap triggering condition
-    #if abs(acc_z_tmp) > 1.0 and abs(gyro_y_tmp) < 50 and abs(gyro_z_tmp) < 20:
-    if abs(acc_z_tmp) > 0.8:
+    if abs(acc_z_tmp) > 0.8 and tof.read() < 300:   # tof: mm
         if tap == 0:
             tap = 1
             tap_wait = 0   # N, unit: N * acc_window_t ms
@@ -117,28 +147,13 @@ while True:
             else:
                 tap_count += 1
                 print("double tap # %d" % tap_count)
+                print("time stamp:", IMU_time_stamp)
+                print(f"Distance: {tof.read()}mm")
                 led.on()
                 tap = 0
                 tap_wait = 0
-                time.sleep_ms(1000)
                 img = sensor.snapshot()
-                file_name = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-                img.save("/tmp/%s.jpeg" % file_name)
+                #file_name = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+                img.save("/tmp/%s.jpeg" % IMU_time_stamp)
+                time.sleep_ms(1000)
                 led.off()
-
-    #print('{}\t{}'.format(window_acc_z[0],window_acc_z[0]), file=open('/tmp/aaa.logs','a+'))
-
-    start_time = time.ticks_ms()
-
-    #print("Accelerometer: x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}".format(accel_x, accel_y, accel_z))
-    #print("Gyroscope:     x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}".format(gyro_x, gyro_y, gyro_z))
-    #print("")
-
-"""
-with open('/tmp/aaa.csv','w') as file:
-        #for line in file:
-            file.write(str(window_acc_z))
-            #line_Str=line_Str.rstrip('\n')
-            #line_Str=line_Str.rstrip('\r')
-file.close()
-"""
